5f61b898660d1002cf42935192b07c6b0b2f5239
madaster97 <adamstrickland97@gmail.com>
2025-07-28 00:21:13 +0100

    Add local / stored status scaffolding

---
+++ README.md
@@ -0,0 +1,41 @@
+# My Marinara Retake
+
+## Possible States
+- Nothing, OR we completed a timer yesterday
+- There is a timer going + what type/time
+- We've completed a timer today + what type/time
+
+### Parralel States
+- Whether we have an active [notification](https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/notifications/create) (by ID)
+- Whether we have an active alarm (by ID)
+
+## Events that can happen
+- You click the main icon
+- You click a specific context menu:
+    - ... TODO
+- You click a desktop notification
+- The app is about to go inactive
+- An alarm you registered has fired
+
+## Edge Cases
+- Browser goes to sleep (so inactive SW), but you want timer to continue
+- Drift over time with 20 second ping?
+
+### Inactivity
+Example:
+- Timer is running, a couple pings go by
+- Extension is about to go inactive
+- Just give up and see what happens! **Count as a pause**
+
+## OKAY!
+APIs needed:
+- Ping loop for active timer
+- Storing state to handle resumptions (like click from pause, could be inactive)
+- Create/Remove notification
+- 
+
+### Constraints
+- (Good?) Having a listener keeps stuff open
+- `onStartup` and `onSuspend` seem to be CHROME open/closing 
+
+**Design the above to persist all state when we stop timer**
--- background.js
+++ background.js
@@ -3,6 +3,70 @@
 // found in the LICENSE file.
 'use strict';
 
+// In-Memory Status: 'asleep','paused','active','break'
+// 'asleep' will cover 2 things:
+// - Startup
+// - Time after a timer completes, but the user has not started the next
+let localStatus = null; 
+// Need to compare status to 'lastStatus' for full context
+let lastStatus = null;
+// Local timer (DateTime), 
+let nextNotify = null; 
+
+// Constants for Stored State - Only to handle inactivity
+// Will assume pause stops ticking, so SW likely to go inactive
+const STORE_PAUSE_LEFTOVER = 'store-pause-leftover'; // milliseconds left on timer
+const STORE_PAUSE_LAST_STATUS = 'store-pause-last-status'; // the last status before pause
+
+// Handle case where 1 click starts to load the stored status, but another click comes in
+// Global promise for "I'm loading the status into localStatus"
+let loading;
+async function loadStatus(){
+  if (localStatus) {
+    // Already loaded
+    return;
+  } else if (loading) {
+    // Someone else loading
+    await loading;
+    return;
+  }
+
+  loading = 
+    chrome.storage.local.get([STORE_PAUSE_LAST_STATUS,STORE_PAUSE_LEFTOVER])
+    .then(results => {
+      // TODO setup localStatus/...
+      // TODO assume they clicked the thing for X reason?
+      // So maybe caller is responsible for processing next tick/state change?
+      console.log('Loaded results: %o', results)
+    })
+    .then(() => chrome.storage.local.remove([STORE_PAUSE_LAST_STATUS,STORE_PAUSE_LEFTOVER]))
+  await loading;
+  loading = null;
+}
+
+//** Operations that could be on startup */
+
+// Handle someone clicking the button in a notification
+// Check stored status because this could be on startup
+async function notificationClickListener() {
+  await loadStatus();
+  // TODO reference localStatus, which is now loaded!
+}
+
+// Handle someone clicking the app icon
+// Check stored status because this could be on startup
+async function iconClickListener() {
+  await loadStatus();
+  // TODO reference localStatus, which is now loaded!
+}
+
+//** Operations only registered after startup */
+// TODO... https://developer.chrome.com/docs/extensions/develop/migrate/to-service-workers#keep_a_service_worker_alive_continuously
+async function runHeartbeat() {
+  await chrome.storage.local.set({ 'last-heartbeat': new Date().getTime() });
+}
+
+// TODO remove/tweak
 chrome.alarms.onAlarm.addListener(() => {
   chrome.action.setBadgeText({ text: '' });
   chrome.notifications.create({
@@ -20,3 +84,6 @@ chrome.notifications.onButtonClicked.addListener(async () => {
   chrome.action.setBadgeText({ text: 'ON' });
   chrome.alarms.create({ delayInMinutes: item.minutes });
 });
+
+// Actually register things
+// TODO - notifications + icon click + contextMenus
